7b5f657c25f23ffbcd565ec50253eed6caeced3c
westongcooper <cooperweston@gmail.com>
2015-08-09 16:31:14 -0400

    fix toggle chef

--- config/routes.rb
+++ config/routes.rb
@@ -40,7 +40,7 @@ Rails.application.routes.draw do
   patch 'chefs/:id' => 'chefs#update'
   put 'chefs/:id' => 'chefs#update'
   delete 'chefs/:id' => 'chefs#destroy'
-  get 'chefs/:id' => 'chefs#toggle', as: 'toggle_active_chef'
+  post 'chefs/:id' => 'chefs#toggle', as: 'toggle_active_chef'
 
   get '/logout', to: 'sessions#destroy'
 
