8016e662093d63d62c51e158f4f631919e812d97
Ben Shelomovitz <bens640@gmail.com>
2015-08-11 16:21:35 -0400

    Working on AWS

--- config/initializers/carrierwave.rb
+++ config/initializers/carrierwave.rb
@@ -4,6 +4,6 @@ CarrierWave.configure do |config|
       aws_access_key_id:     ENV['AWS_KEY'],                        # required
       aws_secret_access_key: ENV['AWS_SECRET'],                        # required
   }
-  config.fog_directory  = 'cookio-pics'                          # required
+  config.fog_directory  = 'cookie-pics'                          # required
   config.fog_public     = false                                        # optional, defaults to true
 end
