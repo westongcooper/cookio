7656a459ceef0fbf3a528473a193823c66cbf982
Ben Shelomovitz <bens640@gmail.com>
2015-08-11 16:01:09 -0400

    Fixed S3 service upload

--- config/initializers/carrierwave.rb
+++ config/initializers/carrierwave.rb
@@ -4,7 +4,6 @@ CarrierWave.configure do |config|
       provider:              'AWS',                        # required
       aws_access_key_id:     ENV['AWS_KEY'],                        # required
       aws_secret_access_key: ENV['AWS_SECRET'],                        # required
-      endpoint:              'https://s3.example.com:8080' # optional, defaults to nil
   }
   config.fog_directory  = 'cookio-pics'                          # required
   config.fog_public     = false                                        # optional, defaults to true
