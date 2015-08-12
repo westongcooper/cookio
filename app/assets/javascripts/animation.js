d14761cd32738bca03dc4fd68fccd31e89435765
westongcooper <cooperweston@gmail.com>
2015-08-12 01:26:57 -0400

    new main page

--- app/assets/javascripts/animation.js
+++ app/assets/javascripts/animation.js
@@ -2,13 +2,14 @@
 
 animate = function(){
   console.log('animate');
+  $('#cookio_animate').addClass('animated bounceInDown');
   $('.collapsible').collapsible({
     accordion : true // A setting that changes the collapsible behavior to expandable instead of the default accordion style
   });
   var options = [
-    {selector: '#bitme', offset: 800, callback: "Materialize.showStaggeredList('#staggered-text')" },
-    {selector: '#bitme', offset: 800, callback: "Materialize.showStaggeredList('#staggered-image')" },
-    {selector: '#bitme', offset: 500, callback: "becomeChef()" }
+    {selector: '#icons_animate', offset: 800, callback: "Materialize.showStaggeredList('#icons_animate')" },
+    {selector: '#how_cookio_animate', offset: 600, callback: "Materialize.fadeInImage('#how_cookio_animate')" },
+    {selector: '#cookio_animate', offset: 0, callback: "becomeChef()" }
   ];
   Materialize.scrollFire(options);
 };
@@ -18,6 +19,8 @@ print_stuff2 = function(){
   console.log('animate');
 };
 becomeChef = function(){
-  Materialize.toast('<span>Now Hiring Chefs!&nbsp;</span><a class=&quot;btn-flat yellow-text&quot; href=/chefs/new> JOIN<a>', 200000)
+  console.log('join us');
+  Materialize.toast('<span>Now Hiring Chefs!&nbsp;</span><a class=&quot;btn-flat yellow-text&quot; href=/chefs/new> Join Us<a>', 10000)
 };
 $(document).on('ready page:load', animate);
+//<a class=close>×<a>
